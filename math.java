5820d784c54aa10b0b87cff0d708247cd623951a
JJJunHa <121237261+JJJunHa@users.noreply.github.com>
2022-12-30 14:41:54 +0900

    Add files via upload

---
+++ math.java
@@ -0,0 +1,47 @@
+
+public class math {
+
+	public static void main(String[] args) {
+		int m1, m2, m3, m4, m5;
+		
+		int[] m = new int[10];
+		
+		m = new int[] {1,2,3,4,5,6,7,8,9,10};
+		
+		
+		
+		
+//		m[0] = 96;
+//		m[1] = 88;
+//		m[2] = 100;
+//		m[3] = 76;
+//		m[4] = 78;
+//		
+//		int i = 0;
+//		while(i<5) {
+//			m[i] = 100;
+//			i++;
+//		}
+		
+//		i=i+1;
+		
+//1~10까지의 합을 구하는 while
+//1~10: 
+		
+		
+	    
+		int i = 0;
+		int total = 0;
+		while(i<=10) {
+			
+			total += (m[i]);
+			System.out.println(total);
+			i++;
+		}
+//		
+		// 배열(Array)
+		
+
+	}
+
+}
